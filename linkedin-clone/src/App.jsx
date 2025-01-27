ce31dba60a42d328f1e12fdd3914484bb7da3ad8
Dan131195 <daniele.franceschini1395@gmail.com>
2025-01-27 12:28:53 +0100

    SideBar

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col lg={8}></Col>
+          <Col lg={4}></Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default App;
