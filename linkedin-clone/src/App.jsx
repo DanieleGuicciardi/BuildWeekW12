a2857af383b67661f15deb8c2d2a0b4b3d5aca07
Federico4820 <federicolepore1@gmail.com>
2025-01-27 14:31:49 +0100

    Footer

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -1,21 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import './App.css'
-import {Container, Row, Col} from 'react-bootstrap';
+import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
+import MyFooter from "./components/MyFooter";
 
 function App() {
-
   return (
     <>
-    
-    <Container>
-    <Row>
-      <Col lg={8}></Col>
-      <Col lg={4}></Col>
-    </Row>
-    </Container>
+      <Container>
+        <Row>
+          <Col lg={8}></Col>
+          <Col lg={4}></Col>
+        </Row>
+        <MyFooter />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
