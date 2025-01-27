ba11a587580f68d1e104ef7dee9843fb06177f1c
Inchino <alessandro.incalza22@gmail.com>
2025-01-27 12:56:03 +0100

    Profile banner

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -2,15 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css'
 import {Container, Row, Col} from 'react-bootstrap';
+import Profile from './components/Profile';
 
 function App() {
 
   return (
     <>
-    
     <Container>
     <Row>
-      <Col lg={8}></Col>
+      <Col lg={8}>
+      <Profile/>
+      </Col>
       <Col lg={4}></Col>
     </Row>
     </Container>
