e4a5d41140c5f15e239d4966fb44476c7faadcf4
Inchino <alessandro.incalza22@gmail.com>
2025-01-28 10:50:26 +0100

    GET e POST

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -3,9 +3,6 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
 import { Container } from "react-bootstrap";
 
-
-//import Sidebar from "./components/Sidebar";
-//import Profile from './components/Profile';
 import MyNavbar from './components/MyNavbar';
 import MyFooter from "./components/MyFooter";
 import AdminArea from "./components/AdminArea";
@@ -18,7 +15,6 @@ function App() {
       <MyNavbar/>
       <Container>
         <AdminArea/>
-        
         <MyFooter />
       </Container>
     </>
