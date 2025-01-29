e71f77e016be542687b96f8407fe2b9253de1bc4
Dan131195 <daniele.franceschini1395@gmail.com>
2025-01-29 10:18:49 +0100

    Home - GET e POST e DELETE

--- linkedin-clone/src/components/Home.jsx
+++ linkedin-clone/src/components/Home.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Alert, Col, Container, Row, Spinner } from "react-bootstrap";
+import HomePosts from "./HomePosts";
 
 function Home() {
   const [data, setData] = useState("");
   const [input, setInput] = useState("");
   const [posts, setPosts] = useState("");
+  const [show, setShow] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [editId, setEditId] = useState(null);
 
   const getPost = async () => {
     try {
@@ -23,6 +27,8 @@ function Home() {
         const datas = result.reverse();
         setPosts(datas.slice(0, 50));
 
+        setIsLoading(false);
+
         console.log("Result: ", result);
       } else {
         throw new Error("Errore nel recupero dati");
@@ -32,33 +38,94 @@ function Home() {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const deletePost = async (id) => {
     try {
       const response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/posts/",
+        `https://striveschool-api.herokuapp.com/api/posts/${id}`,
         {
-          method: "POST",
+          method: "DELETE",
           headers: {
-            "Content-Type": "application/json",
-
             Authorization:
               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk3NDNlZTE2ZjYzNTAwMTVmZWNiN2IiLCJpYXQiOjE3Mzc5NjY1NzQsImV4cCI6MTczOTE3NjE3NH0.ecbfCfnccTYR1ELq9AmO_yfP1Qa1s7IFzSArRl_KadE",
+            "Content-type": "application/json; charset=UTF-8",
           },
-          body: JSON.stringify({ text: input }),
         }
       );
-
       if (response.ok) {
-        const result = await response.json();
-        setData(result);
-        setInput("");
         await getPost();
       } else {
-        throw new Error("Errore nell'inserimento dati");
+        throw new Error("Errore durante l'eliminazione del post.");
       }
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const modifyPost = async (id) => {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/posts/${id}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk3NDNlZTE2ZjYzNTAwMTVmZWNiN2IiLCJpYXQiOjE3Mzc5NjY1NzQsImV4cCI6MTczOTE3NjE3NH0.ecbfCfnccTYR1ELq9AmO_yfP1Qa1s7IFzSArRl_KadE",
+            "Content-type": "application/json; charset=UTF-8",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      if (response.ok) {
+        setEditId(null);
+        setInput("");
+        await getPost();
+      } else {
+        throw new Error("Errore durante la modifica del post.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (editId) {
+      await modifyPost(editId);
+      await getPost();
+    } else {
+      try {
+        const response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/posts/",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk3NDNlZTE2ZjYzNTAwMTVmZWNiN2IiLCJpYXQiOjE3Mzc5NjY1NzQsImV4cCI6MTczOTE3NjE3NH0.ecbfCfnccTYR1ELq9AmO_yfP1Qa1s7IFzSArRl_KadE",
+            },
+            body: JSON.stringify({ text: input }),
+          }
+        );
+
+        if (response.ok) {
+          const result = await response.json();
+          setData(result);
+          setShow(true);
+
+          setTimeout(() => {
+            setShow(false);
+          }, 5000);
+
+          console.log("Dati inseriti: ");
+          setInput("");
+          await getPost();
+        } else {
+          throw new Error("Errore nell'inserimento dati");
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
@@ -76,32 +143,49 @@ function Home() {
             <br />
             <br />
             <br />
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                placeholder="Scrivi qualcosa"
-              />
-              <button type="submit">Invia</button>
+            <form onSubmit={handleSubmit} className="my-4">
+              <div className="bg-white rounded-2 p-2">
+                <h3>Crea un post</h3>
+                <div className="input-group bg-white mt-3">
+                  <input
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    className="form-control"
+                    placeholder="Scrivi qualcosa..."
+                    aria-label="Scrivi qualcosa"
+                  />
+                  <button type="submit" className="btn btn-primary">
+                    {editId ? "Modifica" : "Invia"}
+                  </button>
+                </div>
+              </div>
             </form>
 
-            {data && (
-              <div>
-                <h3>Risposta del server:</h3>
-                <pre>{JSON.stringify(data, null, 2)}</pre>
+            {show && (
+              <Alert variant="success" className="my-2">
+                Post pubblicato con successo !!
+              </Alert>
+            )}
+
+            {isLoading && (
+              <div className="text-center">
+                <Spinner animation="border" variant="primary" />
               </div>
             )}
 
             {posts && (
               <div>
-                {posts.map((e) => (
-                  <div key={e._id} className=" text-break">
-                    <h4>{e.text}</h4>
-                    <p>{e.user.bio}</p>
-                    <p>{e.username}</p>
-                    <hr />
-                  </div>
+                {posts.map((posts) => (
+                  <HomePosts
+                    key={posts._id}
+                    posts={posts}
+                    deletePost={deletePost}
+                    modifyPost={() => {
+                      setEditId(posts._id);
+                      setInput(posts.text);
+                    }}
+                  />
                 ))}
               </div>
             )}
