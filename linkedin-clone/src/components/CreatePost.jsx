7622ba26a5011ae82fd3bb55e91c874d48ff8bac
Dan131195 <daniele.franceschini1395@gmail.com>
2025-01-30 16:39:52 +0100

    GET Post in Profilo

--- linkedin-clone/src/components/CreatePost.jsx
+++ linkedin-clone/src/components/CreatePost.jsx
@@ -1,33 +1,88 @@
-import { Button, Form, Modal, Container } from "react-bootstrap";
-import { useState } from "react";
+import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import Post from "./Post";
+import { Trash } from "react-bootstrap-icons";
+
+const myUsername = "Dani EFFE";
 
 const CreatePost = function () {
   const [posts, setPosts] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [formData, setFormData] = useState({
-    think: "",
-  });
+  // const [showModal, setShowModal] = useState(false);
+  // const [formData, setFormData] = useState({
+  //   think: "",
+  // });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newPost = { id: Date.now(), think: formData.think };
-    setPosts([...posts, newPost]);
-    setFormData({
-      think: "",
-    });
+  const getPost = async () => {
+    try {
+      const response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/posts/",
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk3NDNlZTE2ZjYzNTAwMTVmZWNiN2IiLCJpYXQiOjE3Mzc5NjY1NzQsImV4cCI6MTczOTE3NjE3NH0.ecbfCfnccTYR1ELq9AmO_yfP1Qa1s7IFzSArRl_KadE",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const result = await response.json();
+        const datas = result.reverse();
+        setPosts(datas);
+        console.log("Result: ", datas);
+      } else {
+        throw new Error("Errore nel recupero dati");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  const deletePost = async (id) => {
+    try {
+      const response = await fetch(
+        `https://striveschool-api.herokuapp.com/api/posts/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Nzk3NDNlZTE2ZjYzNTAwMTVmZWNiN2IiLCJpYXQiOjE3Mzc5NjY1NzQsImV4cCI6MTczOTE3NjE3NH0.ecbfCfnccTYR1ELq9AmO_yfP1Qa1s7IFzSArRl_KadE",
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      if (response.ok) {
+        await getPost();
+      } else {
+        throw new Error("Errore durante l'eliminazione del post.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const newPost = { id: Date.now(), think: formData.think };
+  //   setPosts([...posts, newPost]);
+  //   setFormData({
+  //     think: "",
+  //   });
+  // };
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData({ ...formData, [name]: value });
+  // };
 
   return (
     <>
       <Container className="mt-4">
         <div className=" mx-auto bg-white border rounded-3 p-3">
-          <div className="d-flex justify-content-between">
+          {/* <div className="d-flex justify-content-between">
             <h2 className="text-2xl font-semibold mb-6">Post</h2>
             <Button
               variant="link"
@@ -60,11 +115,35 @@ const CreatePost = function () {
                 </Button>
               </Form>
             </Modal.Body>
-          </Modal>
-
-          {posts.map((post) => (
-            <Post key={post.id} post={post} />
-          ))}
+          </Modal> */}
+          <h2>I miei Post</h2>
+          {posts
+            .filter((post) => post.username == myUsername)
+            .map((post) => (
+              <>
+                {/* <Post key={post.id} post={post} /> */}
+                <div className="container mt-4" key={post.id}>
+                  <div className="list-group">
+                    <div className="list-group-item list-group-item-action mb-3">
+                      <div className="w-100">
+                        <p>{post.text}</p>
+                        <div>
+                          {post.image && (
+                            <img src={post.image} className="w-100" />
+                          )}
+                        </div>
+                      </div>
+                      <div>
+                        <Trash
+                          className=" text-danger"
+                          onClick={() => deletePost(post._id)}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ))}
         </div>
       </Container>
     </>
--- linkedin-clone/src/components/Post.jsx
+++ linkedin-clone/src/components/Post.jsx
@@ -4,8 +4,11 @@ const Post = function ({ post }) {
     <div className="container mt-4" key={post.id}>
       <div className="list-group">
         <div className="list-group-item list-group-item-action mb-3">
-          <div className="d-flex w-100 justify-content-between">
-            <p>{post.think}</p>
+          <div className="w-100">
+            <p>{post.text}</p>
+            <div>
+              {post.image && <img src={post.image} className="w-100" />}
+            </div>
           </div>
         </div>
       </div>
